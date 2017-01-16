0c861d17623dc75f70e72520e61b1cbf6bfff195
Yafei <halloweltyan@gmail.com>
2017-01-16 22:12:41 +0100

    change directory name to "java", maven take this as default compiled directory.

--- src/main/java/db/IHibernateSessionFactorySupport.java
+++ src/main/java/db/IHibernateSessionFactorySupport.java
@@ -6,8 +6,8 @@ import org.hibernate.Session;
  * Created by Yafei on 10/01/2017.
  */
 public interface IHibernateSessionFactorySupport {
-    public Session getSession();
-    public void beginTrasaction();
-    public void commitTransaction();
-    public void closeAll();
+    Session getSession();
+    void beginTrasaction();
+    void commitTransaction();
+    void closeAll();
 }
